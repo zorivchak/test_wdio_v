100be2b901080034918aefa91a551ac440509cf1
zorivchak <zorivchak@gmail.com>
2022-07-04 15:57:21 +0300

    add test

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -161,7 +161,11 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
 
 
     
